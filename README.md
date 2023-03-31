a0c7f98f32eaf89fc1f347d70c476d99e281a84c
n-lebel <nicklbch@gmail.com>
2023-03-31 12:05:57 +0100

    eth_inputs, readme

--- README.md
+++ README.md
@@ -6,10 +6,24 @@
 
 First, make sure [rustup](https://rustup.rs) is installed. This project uses a [nightly](https://doc.rust-lang.org/book/appendix-07-nightly-rust.html) version of [Rust](https://doc.rust-lang.org/book/ch01-01-installation.html). The [`rust-toolchain`](rust-toolchain) file will be used by `cargo` to automatically install the correct version.
 
-Create a `.env` file following the `.env.example` template, and populate the relevant fields. Then, simply run the following command to execute the zk-STARK proving algorithm.
+Create a `input.json` file following the `input.example.json` template, and populate the relevant fields. These are as follows:
+
+- `provider`: Ethereum JSON-RPC provider HTTP address
+- `user_address`: address of the user whose asset ownership is being proven
+- `signature`: an ECDSA secp256k1 signature of the Keccak-hashed eth-formatted message, with v = {00, 01} and not {1b, 1c}
+- `message`: a non-formatted string corresponding to the aforementioned signature
+- `block_number`: the block number to prove assets against. "latest" will provide the latest block
+- `expected_balance`: the claimed owned balance. Needs to be smaller or equal to the actual balance
+
+And for proving values of contract slots, add the following fields:
+
+- `contract_address`: the address of the contract
+- `balance_slot`: the slot of the `balances` mapping. The actual slot will be `keccak(abi.encode(address, uint256(balance_slot)))`
+
+Then, simply run the following command to execute the zk-STARK proving algorithm.
 
 ```
-cargo run --release
+cargo run --release -- --input <INPUT_FILE (input.json)> --command <prove_eth OR prove_erc>
 ```
 
 The program outputs a receipt file in `target/proofs/receipt.json`, which contains a seal (the STARK itself), and a journal which is made of the serialized "public inputs": the account trie root, the block hash, and the claimed balance.
---
+++ host/src/ethereum/requests.rs
@@ -0,0 +1,115 @@
+use crate::ethereum::rpc::{get_contract_input, get_native_input};
+use methods::{ERC20_PROOF_ELF, ERC20_PROOF_ID, NATIVE_PROOF_ELF, NATIVE_PROOF_ID};
+use proof_core::proof_inputs::{ContractProofInput, NativeProofInput, ProofInput};
+use serde::{Deserialize, Serialize};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub trait Request {
+    type Input: ProofInput;
+    fn get_user_address(&self) -> &str;
+    fn get_signature(&self) -> &str;
+    fn get_message(&self) -> &str;
+    fn get_expected_balance(&self) -> &u64;
+    fn get_proof_id(&self) -> [u32; 8];
+    fn get_proof_elf(&self) -> &[u8];
+    fn get_proof_input(&self) -> Result<Self::Input>;
+    fn get_description(&self) -> String;
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct NativeRequest {
+    pub provider: String,
+    pub user_address: String,
+    pub block_number: String,
+    pub signature: String,
+    pub message: String,
+    pub expected_balance: u64,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct ContractRequest {
+    pub provider: String,
+    pub user_address: String,
+    pub block_number: String,
+    pub signature: String,
+    pub message: String,
+    pub expected_balance: u64,
+    pub contract_address: String,
+    pub balance_slot: String,
+}
+
+impl Request for NativeRequest {
+    type Input = NativeProofInput;
+
+    fn get_user_address(&self) -> &str {
+        &self.user_address
+    }
+
+    fn get_signature(&self) -> &str {
+        &self.signature
+    }
+
+    fn get_message(&self) -> &str {
+        &self.message
+    }
+
+    fn get_expected_balance(&self) -> &u64 {
+        &self.expected_balance
+    }
+
+    fn get_proof_id(&self) -> [u32; 8] {
+        NATIVE_PROOF_ID
+    }
+
+    fn get_proof_elf(&self) -> &[u8] {
+        &NATIVE_PROOF_ELF
+    }
+
+    fn get_proof_input(&self) -> Result<Self::Input> {
+        get_native_input(self)
+    }
+
+    fn get_description(&self) -> String {
+        String::from("latest account proof")
+    }
+}
+
+impl Request for ContractRequest {
+    type Input = ContractProofInput;
+
+    fn get_user_address(&self) -> &str {
+        &self.user_address
+    }
+
+    fn get_signature(&self) -> &str {
+        &self.signature
+    }
+
+    fn get_message(&self) -> &str {
+        &self.message
+    }
+
+    fn get_expected_balance(&self) -> &u64 {
+        &self.expected_balance
+    }
+
+    fn get_proof_id(&self) -> [u32; 8] {
+        ERC20_PROOF_ID
+    }
+
+    fn get_proof_elf(&self) -> &[u8] {
+        &ERC20_PROOF_ELF
+    }
+
+    fn get_proof_input(&self) -> Result<Self::Input> {
+        get_contract_input(self)
+    }
+
+    fn get_description(&self) -> String {
+        format!(
+            "latest balance slot proof for contract {}",
+            &self.contract_address
+        )
+    }
+}
